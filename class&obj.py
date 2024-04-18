dab25fe5a74ea8a2c479a3003aed274275edfb3b
Srujana-Math <167081923+Srujana-Math@users.noreply.github.com>
2024-04-18 22:42:59 +0530

    classes & obj

---
+++ class&obj.py
@@ -0,0 +1,31 @@
+class Box:
+    def __init__(self,currname,currroll,currmarks):
+       self.name=currname
+       self.roll=currroll
+       self.marks=currmarks
+
+student1=Box("ABC",78,89)
+print(student1.name)
+print(student1.roll)
+print(student1.marks)
+
+student2=Box("EDE",54,68)
+print(student2.name)
+print(student2.roll)
+print(student2.marks)
+
+student3=Box("FKJ",12,78)
+print(student3.name)
+print(student3.roll)
+print(student3.marks)
+
+student4=Box("SDJK",45,56)
+print(student4.name)
+print(student4.roll)
+print(student4.marks)
+
+student5=Box("CJI",25,80)
+print(student5.name)
+print(student5.roll)
+print(student5.marks)
+
